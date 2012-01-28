853d986d4da1c3bf3777089f4623bd915134e7d2
Georgios Lounis <geolo253@gmail.com>
2012-01-28 10:28:51 +0100

    Updated make file script, added new defconfigs

--- make-script.sh
+++ make-script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# $1 is the root directory of your android version, i.e. Gingerbread
-make ARCH=arm star_ifx_defconfig
+# $1 is the defconfig to use in arch/arm/configs
+#make ARCH=arm $1
+make ARCH=arm v20o_geolo253_defconfig
 
-make ARCH=arm CROSS_COMPILE=~/$1/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- STAR_TMUS_REV=TMUS_10 TARGET_STAR_HWREV=TMUS_E TARGET_MODEM=ifx
+make ARCH=arm CROSS_COMPILE=~/Android_source/ICS/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- STAR_TMUS_REV=TMUS_10 TARGET_STAR_HWREV=TMUS_E TARGET_MODEM=ifx
