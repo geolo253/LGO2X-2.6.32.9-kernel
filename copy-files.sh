bd92245957fb0b744f03e64e43ac4ce4599f0101
Georgios Lounis <geolo253@gmail.com>
2012-02-04 22:21:18 +0100

    Added directory latest-build, updated copy-files.sh to copy build files to latest-build

--- copy-files.sh
+++ copy-files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp arch/arm/boot/zImage copied-files/zImage
+cp arch/arm/boot/zImage latest-build/zImage
 
-cp crypto/ansi_cprng.ko copied-files/ansi_cprng.ko
-cp drivers/net/wireless/bcm4329/wireless.ko copied-files/wireless.ko
-cp drivers/scsi/scsi_wait_scan.ko copied-files/scsi_wait_scan.ko
-cp fs/cifs/cifs.ko copied-files/cifs.ko
+cp crypto/ansi_cprng.ko latest-build/ansi_cprng.ko
+cp drivers/net/wireless/bcm4329/wireless.ko latest-build/wireless.ko
+cp drivers/scsi/scsi_wait_scan.ko latest-build/scsi_wait_scan.ko
+cp fs/cifs/cifs.ko latest-build/cifs.ko
